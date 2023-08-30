8b7c2b21642733f3ff7c69b2f97a57d4df3abc0f
Bo Tran <55688614+brass-pixel@users.noreply.github.com>
2023-08-30 10:38:20 +1000

    Finished video 5

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '27 Aug 2023'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '28 Aug 2023'
-    },
-
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
